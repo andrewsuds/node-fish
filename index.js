1fc592c1afb188083df709d9aebf17209055f9da
andrewsuds <andrewsuds@gmail.com>
2022-06-26 02:03:08 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://fish-l0pa.onrender.com"],
+    origin: "https://fish-l0pa.onrender.com",
     credentials: true,
     exposedHeaders: ["set-cookie"],
   })
