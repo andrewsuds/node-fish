167d53b71e4b70d3f539f483e62ccf198a2fd15b
andrewsuds <andrewsuds@gmail.com>
2022-06-26 02:22:47 -0400

    Changed Origin

--- index.js
+++ index.js
@@ -12,9 +12,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://fishbucket.netlify.app"],
+    origin: [
+      "http://localhost:3000",
+      "https://fishbucket.netlify.app",
+      "https://fish.aks22.com",
+    ],
     credentials: true,
-    exposedHeaders: ["set-cookie"],
   })
 );
 
