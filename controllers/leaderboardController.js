86bead0dac97a67f88eb568a964093692fe91e9e
andrewsuds <andrewsuds@gmail.com>
2022-07-01 01:27:24 -0400

    Updated Leaderboard Controller

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -1,103 +1,68 @@
 const pool = require("../helpers/db");
 
-function getTotalWeight(req, res) {
-  pool.query(
-    `SELECT username, sum(weight) as weight FROM post
+async function getTotalWeight(req, res) {
+  const weightQuery = (hours) => {
+    return `SELECT username, sum(weight) as weight FROM post
     INNER JOIN account on post.accountid = account.accountid
-    WHERE postdate >= NOW()-INTERVAL '24 HOURS'
+    WHERE postdate >= NOW()-INTERVAL '${hours} HOURS'
     group by username
-    order by weight DESC;`,
-    (err, result) => {
-      if (err) {
-        console.log(err.message);
-        return res.json({ message: err.message });
-      }
-
-      if (result) {
-        return res.json(result.rows);
-      }
-    }
-  );
+    order by weight DESC;`;
+  };
+  const day = await pool.query(weightQuery(24));
+  const weekend = await pool.query(weightQuery(72));
+  return res.json({ day: day.rows, weekend: weekend.rows });
 }
 
-function getTotalCaught(req, res) {
-  pool.query(
-    `SELECT username, count(postid) as caught FROM post
+async function getTotalCaught(req, res) {
+  const caughtQuery = (hours) => {
+    return `SELECT username, count(postid) as caught FROM post
     INNER JOIN account on post.accountid = account.accountid
-    WHERE postdate >= NOW()-INTERVAL '24 HOURS'
+    WHERE postdate >= NOW()-INTERVAL '${hours} HOURS'
     group by username
-    order by caught desc;`,
-    (err, result) => {
-      if (err) {
-        console.log(err.message);
-        return res.json({ message: err.message });
-      }
-
-      if (result) {
-        return res.json(result.rows);
-      }
-    }
-  );
+    order by caught desc;`;
+  };
+  const day = await pool.query(caughtQuery(24));
+  const weekend = await pool.query(caughtQuery(72));
+  return res.json({ day: day.rows, weekend: weekend.rows });
 }
 
-function getBiggest(req, res) {
-  pool.query(
-    `SELECT username, max(weight) as weight FROM post
+async function getBiggest(req, res) {
+  const biggestQuery = (hours) => {
+    return `SELECT username, max(weight) as weight FROM post
       INNER JOIN account on post.accountid = account.accountid
-      WHERE postdate >= NOW()-INTERVAL '24 HOURS'
+      WHERE postdate >= NOW()-INTERVAL '${hours} HOURS'
       group by username
-      order by weight DESC;`,
-    (err, result) => {
-      if (err) {
-        console.log(err.message);
-        return res.json({ message: err.message });
-      }
-
-      if (result) {
-        return res.json(result.rows);
-      }
-    }
-  );
+      order by weight DESC;`;
+  };
+  const day = await pool.query(biggestQuery(24));
+  const weekend = await pool.query(biggestQuery(72));
+  return res.json({ day: day.rows, weekend: weekend.rows });
 }
 
-function getSmallest(req, res) {
-  pool.query(
-    `SELECT username, min(weight) as weight FROM post
+async function getSmallest(req, res) {
+  const smallestQuery = (hours) => {
+    return `SELECT username, min(weight) as weight FROM post
       INNER JOIN account on post.accountid = account.accountid
-      WHERE postdate >= NOW()-INTERVAL '24 HOURS'
+      WHERE postdate >= NOW()-INTERVAL '${hours} HOURS'
       group by username
-      order by weight ASC;`,
-    (err, result) => {
-      if (err) {
-        console.log(err.message);
-        return res.json({ message: err.message });
-      }
-
-      if (result) {
-        return res.json(result.rows);
-      }
-    }
-  );
+      order by weight ASC;`;
+  };
+  const day = await pool.query(smallestQuery(24));
+  const weekend = await pool.query(smallestQuery(72));
+  return res.json({ day: day.rows, weekend: weekend.rows });
 }
 
-function getLongest(req, res) {
-  pool.query(
-    `SELECT username, max(length) as length FROM post
+async function getLongest(req, res) {
+  const longestQuery = (hours) => {
+    return `SELECT username, max(length) as length FROM post
     INNER JOIN account on post.accountid = account.accountid
-    WHERE postdate >= NOW()-INTERVAL '24 HOURS'
+    WHERE postdate >= NOW()-INTERVAL '${hours} HOURS'
     group by username
-    order by length DESC;`,
-    (err, result) => {
-      if (err) {
-        console.log(err.message);
-        return res.json({ message: err.message });
-      }
-
-      if (result) {
-        return res.json(result.rows);
-      }
-    }
-  );
+    order by length DESC;`;
+  };
+  const day = await pool.query(longestQuery(24));
+  const weekend = await pool.query(longestQuery(72));
+  return res.json({ day: day.rows, weekend: weekend.rows });
 }
 
 module.exports = {
