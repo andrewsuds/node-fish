04a5884d4bbe7534738ee06fa24be083cf3c0e17
andrewsuds <andrewsuds@gmail.com>
2022-07-03 22:20:56 -0400

    Changed Leaderboard Controller Logic

--- controllers/leaderboardController.js
+++ controllers/leaderboardController.js
@@ -1,68 +1,102 @@
 const pool = require("../helpers/db");
 
-async function getTotalWeight(req, res) {
-  const weightQuery = (hours) => {
-    return `SELECT username, sum(weight) as weight FROM post
+function getTotalWeight(req, res) {
+  const hours = req.params.hours;
+  pool.query(
+    `SELECT username, CONCAT(sum(weight), ' lbs') as value FROM post
     INNER JOIN account on post.accountid = account.accountid
     WHERE postdate >= NOW()-INTERVAL '${hours} HOURS'
     group by username
-    order by weight DESC;`;
-  };
-  const day = await pool.query(weightQuery(24));
-  const weekend = await pool.query(weightQuery(72));
-  return res.json({ day: day.rows, weekend: weekend.rows });
+    order by value DESC;`,
+    (err, result) => {
+      if (err) {
+        return res.status(400).json("Error");
+      }
+      if (result) {
+        return res.json(result.rows);
+      }
+    }
+  );
 }
 
-async function getTotalCaught(req, res) {
-  const caughtQuery = (hours) => {
-    return `SELECT username, count(postid) as caught FROM post
+function getTotalCaught(req, res) {
+  const hours = req.params.hours;
+  pool.query(
+    `SELECT username, CONCAT(count(postid), ' fish') as value FROM post
     INNER JOIN account on post.accountid = account.accountid
     WHERE postdate >= NOW()-INTERVAL '${hours} HOURS'
     group by username
-    order by caught desc;`;
-  };
-  const day = await pool.query(caughtQuery(24));
-  const weekend = await pool.query(caughtQuery(72));
-  return res.json({ day: day.rows, weekend: weekend.rows });
+    order by value desc;`,
+    (err, result) => {
+      if (err) {
+        return res.status(400).json("Error");
+      }
+
+      if (result) {
+        return res.json(result.rows);
+      }
+    }
+  );
 }
 
-async function getBiggest(req, res) {
-  const biggestQuery = (hours) => {
-    return `SELECT username, max(weight) as weight FROM post
+function getBiggest(req, res) {
+  const hours = req.params.hours;
+  pool.query(
+    `SELECT username, CONCAT(max(weight), ' lbs') as value FROM post
       INNER JOIN account on post.accountid = account.accountid
       WHERE postdate >= NOW()-INTERVAL '${hours} HOURS'
       group by username
-      order by weight DESC;`;
-  };
-  const day = await pool.query(biggestQuery(24));
-  const weekend = await pool.query(biggestQuery(72));
-  return res.json({ day: day.rows, weekend: weekend.rows });
+      order by value DESC;`,
+    (err, result) => {
+      if (err) {
+        return res.status(400).json("Error");
+      }
+
+      if (result) {
+        return res.json(result.rows);
+      }
+    }
+  );
 }
 
-async function getSmallest(req, res) {
-  const smallestQuery = (hours) => {
-    return `SELECT username, min(weight) as weight FROM post
+function getSmallest(req, res) {
+  const hours = req.params.hours;
+  pool.query(
+    `SELECT username, CONCAT(min(weight), ' lbs') as value FROM post
       INNER JOIN account on post.accountid = account.accountid
       WHERE postdate >= NOW()-INTERVAL '${hours} HOURS'
       group by username
-      order by weight ASC;`;
-  };
-  const day = await pool.query(smallestQuery(24));
-  const weekend = await pool.query(smallestQuery(72));
-  return res.json({ day: day.rows, weekend: weekend.rows });
+      order by value ASC;`,
+    (err, result) => {
+      if (err) {
+        return res.status(400).json("Error");
+      }
+
+      if (result) {
+        return res.json(result.rows);
+      }
+    }
+  );
 }
 
-async function getLongest(req, res) {
-  const longestQuery = (hours) => {
-    return `SELECT username, max(length) as length FROM post
+function getLongest(req, res) {
+  const hours = req.params.hours;
+  pool.query(
+    `SELECT username, CONCAT(max(length), ' in.') as value FROM post
     INNER JOIN account on post.accountid = account.accountid
     WHERE postdate >= NOW()-INTERVAL '${hours} HOURS'
     group by username
-    order by length DESC;`;
-  };
-  const day = await pool.query(longestQuery(24));
-  const weekend = await pool.query(longestQuery(72));
-  return res.json({ day: day.rows, weekend: weekend.rows });
+    order by value DESC;`,
+    (err, result) => {
+      if (err) {
+        return res.status(400).json("Error");
+      }
+
+      if (result) {
+        return res.json(result.rows);
+      }
+    }
+  );
 }
 
 module.exports = {
--- routes/leaderboardRouter.js
+++ routes/leaderboardRouter.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const leaderboardController = require("../controllers/leaderboardController");
 const checkMiddleware = require("../middlewares/checkMiddleware");
 
-router.get("/total-weight", leaderboardController.getTotalWeight);
-router.get("/total-caught", leaderboardController.getTotalCaught);
-router.get("/biggest", leaderboardController.getBiggest);
-router.get("/smallest", leaderboardController.getSmallest);
-router.get("/longest", leaderboardController.getLongest);
+router.get("/total-weight/:hours", leaderboardController.getTotalWeight);
+router.get("/total-caught/:hours", leaderboardController.getTotalCaught);
+router.get("/biggest/:hours", leaderboardController.getBiggest);
+router.get("/smallest/:hours", leaderboardController.getSmallest);
+router.get("/longest/:hours", leaderboardController.getLongest);
 module.exports = router;
