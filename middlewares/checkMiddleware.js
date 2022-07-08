00c3f768dd3454d0bbc5414b0ce0eff0bb724353
andrewsuds <andrewsuds@gmail.com>
2022-07-08 00:35:15 -0400

    Updated Middleware

--- middlewares/checkMiddleware.js
+++ middlewares/checkMiddleware.js
@@ -1,6 +1,8 @@
 function checkMiddleware(req, res, next) {
   if (!req.session || !req.session.accountid) {
-    return res.json({ loggedIn: false, message: "You are not logged in!" });
+    return res
+      .status(400)
+      .json({ loggedIn: false, message: "You are not logged in!" });
   }
   next();
 }
