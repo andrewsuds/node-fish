8625822ce038daad44759576abf360b225df35af
andrewsuds <andrewsuds@gmail.com>
2022-07-15 00:05:24 -0400

    Changed Auth Middleware

--- middlewares/checkMiddleware.js
+++ middlewares/checkMiddleware.js
@@ -1,8 +1,6 @@
 function checkMiddleware(req, res, next) {
   if (!req.session || !req.session.accountid) {
-    return res
-      .status(400)
-      .json({ loggedIn: false, message: "You are not logged in!" });
+    return res.json({ loggedIn: false, message: "You are not logged in!" });
   }
   next();
 }
